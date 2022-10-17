37f25d80bc7b52b5971cc75ffc9dd766d75a8478
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-10-17 18:11:07 -0500

    active listos

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
+//elemento html
 let input = document.getElementById("myToggle")
 let Card_Price = document.getElementsByClassName("card_price")
 
 
-console.log(Card_Price)
+//Evento click
 input.addEventListener("click",input_able) 
 
+
+
+//json precios
 const price_monty = [19.99,24.99,39.99]
 const price_annually = [199.99,249.99,399.99]
 
 
+
+
+//funcion que inserta precios
 function input_able() {
 
     if(input.checked==true){
@@ -20,10 +27,5 @@ function input_able() {
         for (const key in Card_Price) {
             Card_Price[key].innerHTML = `$${price_annually[key]} `
         }
-
-        console.log("precio anual")
-
     }
-
-
 }
