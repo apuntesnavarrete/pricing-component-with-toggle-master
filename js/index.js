e373a76c7acdec8b3375473fa530c062b33449cc
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-10-18 18:59:45 -0500

    identar codigo y terminar

--- js/index.js
+++ js/index.js
@@ -1,31 +1,23 @@
 //elemento html
-let input = document.getElementById("myToggle")
-let Card_Price = document.getElementsByClassName("card_price")
-
+let input = document.getElementById("myToggle");
+let Card_Price = document.getElementsByClassName("card_price");
 
 //Evento click
-input.addEventListener("click",input_able) 
-
-
+input.addEventListener("click", input_able);
 
 //json precios
-const price_monty = [19.99,24.99,39.99]
-const price_annually = [199.99,249.99,399.99]
-
-
-
+const price_monty = [19.99, 24.99, 39.99];
+const price_annually = [199.99, 249.99, 399.99];
 
 //funcion que inserta precios
 function input_able() {
-
-    if(input.checked==true){
-        for (const key in Card_Price) {
-            Card_Price[key].innerHTML = `$${price_monty[key]} `
-        }
-
-    } else {
-        for (const key in Card_Price) {
-            Card_Price[key].innerHTML = `$${price_annually[key]} `
-        }
+  if (input.checked == true) {
+    for (const key in Card_Price) {
+      Card_Price[key].innerHTML = `$${price_monty[key]} `;
     }
+  } else {
+    for (const key in Card_Price) {
+      Card_Price[key].innerHTML = `$${price_annually[key]} `;
+    }
+  }
 }
